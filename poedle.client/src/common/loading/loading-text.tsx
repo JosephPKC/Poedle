333e1e014117e199887678297f841c035fa832e0
Joseph Park <josephpkc@gmail.com>
2024-08-27 18:57:56 -0700

    Remove ConsoleTester, fix some text

--- poedle.client/src/common/loading/loading-text.tsx
+++ poedle.client/src/common/loading/loading-text.tsx
@@ -1,5 +1,5 @@
 export function LoadingText() {
     return (
-        <p>Fetching data. Please wait or refresh after a few seconds.</p>
+        <p className="txt-center txt-outer">Fetching data. Please wait or refresh after a few seconds.</p>
     );
 }
